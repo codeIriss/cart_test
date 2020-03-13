9ec2ee44692967690b9a7479bb8945e8dc5e23e4
codeIriss <goal.matters.20@gmail.com>
2020-03-14 00:02:16 +0530

    Updates

---
+++ precache-manifest.3f76dccd23ba626e17c896c32e6c3191.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86d6832af3419881cbcfa269b8b6ba5e",
+    "url": "/cart_test/index.html"
+  },
+  {
+    "revision": "2bbad0b2473b2f059724",
+    "url": "/cart_test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bb0469d2ce91fa0f5e79",
+    "url": "/cart_test/static/js/2.59c59637.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/cart_test/static/js/2.59c59637.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bbad0b2473b2f059724",
+    "url": "/cart_test/static/js/main.f29474bc.chunk.js"
+  },
+  {
+    "revision": "b8d5531b52f0d2f0f402",
+    "url": "/cart_test/static/js/runtime-main.6ccc4ec7.js"
+  }
+]);
